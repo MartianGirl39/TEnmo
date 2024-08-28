dfe197f26f8c47ea938ca5c8fe86e163cfb32146
Seth Rakich <seth.r.drummage@gmail.com>
2024-08-28 11:59:24 -0400

    AccountDao TransferDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,5 +1,50 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountDao {
 
-}
+    private JdbcTemplate jdbcTemplate;
+
+    public AccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Account getAccountById(int id) {
+        Account account = new Account();
+        String sql = "SELECT * from account where account_id = ?; ";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+
+        if (results.next()) {
+            account = mapRowToAccount(results);
+        }
+        return account;
+    }
+
+    public List<Account> listUser() {
+        List<Account> account = new ArrayList<>();
+        String sql = "SELECT * from account;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+
+        while (results.next()) {
+            account.add(mapRowToAccount(results));
+        }
+        return account;
+
+    }
+
+    private Account mapRowToAccount(SqlRowSet rs) {
+        Account Account = new Account();
+        Account.setUser_id(rs.getInt("user_id"));
+        Account.setAccount_id(rs.getInt("account_id"));
+        Account.setBalance(rs.getDouble("balance"));
+
+        return Account;
+
+    }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,4 +1,69 @@
 package com.techelevator.tenmo.dao;
 
+
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransferDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public TransferDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Transfer getTransferById(int id) {
+        Transfer transfer = new Transfer();
+        String sql = "Select * from transfer where transfer_id = ? ;";
+
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
+
+        if (result.next()) {
+            transfer = mapRowToTransfer(result);
+        }
+        return transfer;
+    }
+    public List<Transfer> getTransferByUser(int user_id){
+        List<Transfer> transfers = new ArrayList<>();
+        String sql = " select * from transfer where account_from = ? OR account_to = ? ;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,user_id,user_id);
+
+        while (results.next() ){
+            transfers.add(mapRowToTransfer(results) );
+        }
+        return transfers;
+
+    }
+    public int sendTeBucks(int senderId, int receiverId, double amount){
+        int id = 0;
+        String sql = "insert into transfer (transfer_type_id,transfer_status_id,account_from,account_to,amount) values(2,2,?,?,?) returning transfer_id;";
+
+        id = jdbcTemplate.queryForObject(sql,int.class,senderId,receiverId,amount);
+        return id;
+
+    }
+    public int requestTeBucks(int senderId, int receiverId, double amount) {
+        int id = 0;
+        String sql = "insert into transfer (transfer_type_id,transfer_status_id,account_from,account_to,amount) values(1,1,?,?,?) returning transfer_id;";
+
+        id = jdbcTemplate.queryForObject(sql, int.class, senderId, receiverId, amount);
+        return id;
+    }
+
+    private Transfer mapRowToTransfer(SqlRowSet rs) {
+        Transfer Transfer = new Transfer();
+        Transfer.setTransfer_id(rs.getInt("transfer_id"));
+        Transfer.setTransfer_type_id(rs.getInt("transfer_type_id"));
+        Transfer.setTransfer_status_id(rs.getInt("transfer_status_id"));
+        Transfer.setAccount_from(rs.getInt("account_from"));
+        Transfer.setAccount_to(rs.getInt("account_to"));
+        Transfer.setAmount(rs.getDouble("amount"));
+        return Transfer;
+
+    }
+
 }
