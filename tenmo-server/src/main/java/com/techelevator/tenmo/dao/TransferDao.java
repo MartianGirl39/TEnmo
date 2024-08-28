46c99a81e255680082ae2bb40f30a8ac3ab5d4ea
Seth Rakich <seth.r.drummage@gmail.com>
2024-08-28 15:27:43 -0400

    Tag

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -27,24 +27,39 @@ public class TransferDao {
         return transfer;
     }
 
-    public List<Transfer> getTransferByUser(int user_id){
+    public List<Transfer> getTransferByUser(int user_id) {
         List<Transfer> transfers = new ArrayList<>();
         String sql = " select * from transfer where account_from = ? OR account_to = ? ;";
 
-        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,user_id,user_id);
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, user_id, user_id);
 
-        while (results.next() ){
-            transfers.add(mapRowToTransfer(results) );
+        while (results.next()) {
+            transfers.add(mapRowToTransfer(results));
         }
         return transfers;
 
     }
 
-    public int sendTeBucks(int senderId, int receiverId, double amount){
+    public List<Transfer> getTransfersByType(int user_id, String type ) {
+        List<Transfer> transferTypeList = new ArrayList<>();
+
+        String sql = " select transfer_id, transfer_type_id,transfer_status_id,account_from,account_to,amount from transfer t join transfer_type tt on tt.transfer_type_id = t.transfer_type_id where user_id = ? and transfer_type_desc = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, user_id, type);
+
+        while (results.next()) {
+            transferTypeList.add(mapRowToTransfer(results));
+        }
+        return transferTypeList;
+
+
+    }
+
+    public int sendTeBucks(int senderId, int receiverId, double amount) {
         int id = 0;
         String sql = "insert into transfer (transfer_type_id,transfer_status_id,account_from,account_to,amount) values(2,2,?,?,?) returning transfer_id;";
 
-        id = jdbcTemplate.queryForObject(sql,int.class,senderId,receiverId,amount);
+        id = jdbcTemplate.queryForObject(sql, int.class, senderId, receiverId, amount);
         return id;
     }
 
@@ -54,6 +69,9 @@ public class TransferDao {
 
         id = jdbcTemplate.queryForObject(sql, int.class, senderId, receiverId, amount);
         return id;
+    }
+    public void updateTransactionStatus(Transfer transfer){
+        String sql = "update transfer"
     }
 
     private Transfer mapRowToTransfer(SqlRowSet rs) {
